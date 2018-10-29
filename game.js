5ab4d41face86ebea2999a9f7056d148f6a09200
Cory Cotterell <cordawg123@gmail.com>
2018-10-28 20:44:02 -0600

    added automation to divs and made the game work

--- game.js
+++ game.js
@@ -1,160 +1,119 @@
 //GAME FILE\
-// const md5 = require("md5")
-// const axios = require("axios")
-const publicKey = "09714c36f109af79663ce09d4dbb60d7fff7368af77c062e423e0f58fa46b9e7";
-const privateKey = "6a4c1b7915c93fe11001dc8905558d73a1237489";
-// const ts = Date.now()
-// const hash = md5(ts + privateKey + publicKey);
-
-const url = `https://api.unsplash.com/search/photos/?query=cat`;//generates photos based on search
-const imgDivs = document.querySelectorAll(".box")
-
-
-//user clicks on a card 
-//click reduces flip by 1 
-//when flip is at 0 
-//checks for match, if match keep flipped
-//else reapply visible:hidden
-
-// function cardFlipper(){
-//   if(flips>0){
-//     flips = flips - 1;
-//   }
-//   else if(!match && flips === 0){
-//     findImage.classList.remove("show");
-//   }
-// }
-let flips = 0;
-let urlCompareArray = []
-let card1Flipped = null
-let card2Flipped = null
+let card1Flipped = ""
+let card2Flipped = ""
 let lastSelecetedDiv = ""
 let numberOfFlips = 0;
-numOfMatches = 0;
+let pic1
+let pic2
+let numOfMatches = 0;
+const matchTotal = 10;
 
-//no match
-//fast clickng prevention
-//same clicking prevention
-const matchChecker = function(){
-  debugger
-  console.log(card1Flipped)
-  //console.log(card1Flipped);
-  if(card1Flipped.getAttribute("src") === card2Flipped.getAttribute("src")){
-    numOfMatches = numOfMatches + 1;
-    console.log("It's a match!")
-    numberOfFlips = 0;
-    card1Flipped = null
-    card2Flipped = null
-    return;
-  }else{
-    debugger
-    card1Flipped.classList.remove("show");
-    card2Flipped.classList.remove("show");
-    card1Flipped = null
-    card2Flipped = null
-    numberOfFlips = 0;
-  }
-
-  //no match
-  //check if 1 match is made
-  //check if 10 matchees are made
-  matches = 0
+const wonGameAlert = function(){ // checks to see if all matches are complete if so alerts user telling them they have won the game
+  setTimeout(()=>{
+    if(numOfMatches === matchTotal){
+      alert("You have won the game congrats man :D")
+    }
+  },1000)
 }
-
-const sameCardClicked = (e)=>{
+const matchChecker = function(matches){
+  if(card1Flipped === card2Flipped){ //Checks to see if first selected card is the same as second selected card
+    numOfMatches = numOfMatches + 1;
+    numberOfFlips = 0;
+    card1Flipped = ""
+    card2Flipped = ""
+    wonGameAlert()
+    return;
+  }else{ //If first card selected isn't equal to card 2 resets card 
+    setTimeout(()=>{
+      pic1.classList.remove("show");
+      pic2.classList.remove("show");
+      card1Flipped = ""
+      card2Flipped = ""
+      numberOfFlips = 0;
+    },2000)
+  }
+}
+const sameCardClicked = (e)=>{ // Checks to see if same card is clicked
   if(lastSelecetedDiv === e.target.id) {
     return true;
   }
 }
 const imageDisplay = function(e) {
-  console.log(e)
-  //matchChecker();
-  const findImage = e.target
-
-  if(numberOfFlips === 2 ){//handles fast clicking
+  if(e.target.firstChild.localName !== "img"){ //Prevents Game from breaking if user clicks on something other than image box
+    return;
+  }
+  const findImage = e.target.firstChild
+  if(numberOfFlips === 2 || sameCardClicked(e)){//handles fast clicking
+    return;
+  }
+  if(!card1Flipped.length) { // checks to see if a card is fliped if not assigns card image value then stores that value into variable
+    numberOfFlips = numberOfFlips + 1;
+    pic1 = findImage
+    findImage.classList.add("show");
+    card1Flipped = findImage.getAttribute("src")
+    return;
+  }
+  if(!card2Flipped.length){ // checks to see if a card is fliped if not assigns card image value then stores that value into variable
+    numberOfFlips = numberOfFlips + 1;
+    pic2 = findImage
+    findImage.classList.add("show");
+    card2Flipped = findImage.getAttribute("src")
     matchChecker()
     return;
   }
-  if(!card1Flipped) {
-    numberOfFlips = numberOfFlips + 1;
-    findImage.classList.add("show");
-    card1Flipped = findImage.firstChild
-    console.log(card1Flipped)
-    return;
-  }
-  if(!card2Flipped){
-    numberOfFlips = numberOfFlips + 1;
-    findImage.classList.add("show");
-    card2Flipped = findImage.firstChild
-    matchChecker()
-    return;
-  }
- 
-  // need a card
-  
-  console.log("We're here!")
-  // if (flips > 0) { //user cliicks for the first time
-  //   urlCompareArray.push(findImage.getAttribute("src"))
-  //   console.log(urlCompareArray)
-  //   findImage.classList.add("show")
-  //   flips = flips - 1;
-  // }
-  // if (flips === 0 && urlCompareArray[0] === urlCompareArray[1]) {
-  //   console.log("that's a match!")
-  //   flips = 2
-  //   urlCompareArray = [];
-  // } else if (flips === 0 && urlCompareArray[0] !== urlCompareArray[1]) {
-  //   console.log("It's not a match, :(")
-  //   setTimeout(() => {
-  //     console.log(card1)
-  //     console.log(document.querySelector("img")) -
-  //       document.querySelector("img").classList.remove("show")
-  //     findImage.classList.remove("show")
-  //   }, 1500)
-  //   //findImage.classList.remove("show")
-  //   urlCompareArray = [];
-  //   flips = 2
-
-  //   // else  {
-  //   //   findImage.classList.remove("show")
-  //   //   urlCompareArray=[];
-  //   //   flips = 2
-  //   // }
-  // }
-
 }
+const publicKey = "09714c36f109af79663ce09d4dbb60d7fff7368af77c062e423e0f58fa46b9e7";
 
-axios.get(url,
-  {
-    headers: {
-      Authorization: "Client-ID 09714c36f109af79663ce09d4dbb60d7fff7368af77c062e423e0f58fa46b9e7" //allows our api access
-    }, params: {
-      orientation: "squarish"
+const url = `https://api.unsplash.com/search/photos/?query=mark`;//generates photos based on search
+
+// const imgDivs = document.querySelectorAll(".box")
+const grabImagesFromApi = function(){
+  const imgDivs = document.querySelectorAll(".box")
+  axios.get(url,
+    {
+      headers: {
+        Authorization: "Client-ID 09714c36f109af79663ce09d4dbb60d7fff7368af77c062e423e0f58fa46b9e7" //allows our api access
+      }, params: {
+        orientation: "squarish"
+      }
     }
+  ).then(function (obj) {
+
+    const result = obj.data.results //selects search results
+    const resultCopy = result;
+    const a = new Array(...resultCopy, ...result);
+
+    a.sort(function () { return 0.5 - Math.random() });
+    const grid = document.querySelector(".grid");
+
+    // need click input
+    // the user clicks on div and a single image is dsiplayed
+    grid.addEventListener("click", imageDisplay)
+
+
+    a.forEach((element, index) => {
+      imgDivs[index].innerHTML = `<img src="${element.urls.small}">`
+
+
+    });
+    
+
+  })
+}
+const numberOfBoxess = 20;
+const gameBoard = function(value){ //creates the amount of squares based on original value set
+  for (i = 0; i < value; i++) { 
+    let div = document.createElement("div")
+    div.classList.add("box")
+    let id = document.createAttribute("id")
+    id.value = "1"
+    div.setAttributeNode(id)
+    const findGridElement = document.getElementById("img")
+    findGridElement.appendChild(div)
   }
-).then(function (obj) {
-
-  const result = obj.data.results //selects search results
-  const resultCopy = result;
-  const a = new Array(...resultCopy, ...result);
-
-  a.sort(function () { return 0.5 - Math.random() });
-  const grid = document.querySelector(".grid");
-
-  // need click input
-  // the user clicks on div and a single image is dsiplayed
-  grid.addEventListener("click", imageDisplay)
-
-
-  a.forEach((element, index) => {
-    imgDivs[index].innerHTML = `<img src="${element.urls.small}">`
-
-
-  });
-  
-
-})
-
+  grabImagesFromApi()
+}
+gameBoard(numberOfBoxess)
 
 
 
